919b0ae17bfcce13d72c7f935f28b9afbfc00a4c
root <Joshcones1253@yahoo.com>
2023-05-25 11:12:48 -0700

    J

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - A program that print a line using the printf function
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n")
-return (0)
+printf("with proper grammer, but the outcome is a piece of art,\n:");
+return (0);
 }
