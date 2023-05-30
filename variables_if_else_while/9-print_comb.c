ddb8d2d38d0c99da7819f382d641e501cb9a1be3
root <Joshcones1253@yahoo.com>
2023-05-29 18:17:44 -0700

    RAIDERS

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - prints the lowercase alphabet in reverse.
- * followed by a new line
- * Return: Always 0 (Success)
- */
+* main - prints the lowercase alphabet in reverse.
+* followed by a new line
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int r;
 
 for (r = 0; r < 10; r++)
-
+{
 putchar(r + '0');
 if (r < 9)
 {
 putchar(',');
 putchar(' ');
-
+}
+}
 putchar('\n');
+
 return (0);
 }
