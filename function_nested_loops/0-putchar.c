846aa0ce0fffc3e1cd110bad02abfac4fc146f3a
root <Joshcones1253@yahoo.com>
2023-05-30 13:33:28 -0700

    RAMS

---
+++ function_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: William A Meilahn
+ * Desc:Header file containing declarations for all functions
+ * used in the functions_nested_loops directory.
+ */
+
+int _putchar (char c);
+void print_alphabet(voiid);
+void print_alphabet_*10(void);
+int _islower(int c);
+int -isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
