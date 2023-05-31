24ed25cf138e20d75c0b5bc025990150c8f92005
root <Joshcones1253@yahoo.com>
2023-05-31 10:49:04 -0700

    JAGS

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * 0c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int_isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
