62a42b84e7e741fc66e5cdd41f109d99b72562b2
root <Joshcones1253@yahoo.com>
2023-06-05 14:18:24 -0700

    HOU

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,19 +6,22 @@
  */
 void print_diagonal(int n)
 {
-        int k, f;
-        if (n <= 0)
-        {
-        _putchar('\n');
-        }
-        else
-        {
-        for (k = 0; k < n; k++)
-        {
-        for (f = 0; f < k; f++)
-        {
-        _putchar(' ');
-        }
-        _putchar('\\');
-        _putchar('\n');
-        }
+	int k, f;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (k = 0; k < n; k++)
+		{
+			for (f = 0; f < k; f++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
