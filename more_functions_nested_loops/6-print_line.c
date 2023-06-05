f981357f3d929301f2a3c99df372ea278a9d14d4
root <Joshcones1253@yahoo.com>
2023-06-05 13:48:27 -0700

    KJ

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,17 @@
  */
 void print_line(int n)
 {
-        int k;
-        if (n <= 0)
-        _putchar('\n');
-        else
+	int k;
+	if (n <= 0)
+
+	_putchar('\n');
+
+	else
 {
-        for (k = 0; k < n; k++)
-        _putchar('_');
-        _putchar('\n');
+	for (k = 0; k < n; k++)
+
+	_putchar('_');
+
+	_putchar('\n');
 }
 }
