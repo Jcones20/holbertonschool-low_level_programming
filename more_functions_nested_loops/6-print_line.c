42489c22f157092d3cc37e862c7fc8cad225221f
root <Joshcones1253@yahoo.com>
2023-06-05 13:14:33 -0700

    eagles

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ * @n: line
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+        int k;
+        if (n <= 0)
+        _putchar('\n');
+        else
+{
+        for (k = 0; k < n; k++)
+        _putchar('_');
+        _putchar('\n');
+}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,3 +10,7 @@ void print_square(int size);
 void print_triangle(int size);
 int _putchar(char c);
 void print_most_numbers(void);
+void print_square(int size);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
