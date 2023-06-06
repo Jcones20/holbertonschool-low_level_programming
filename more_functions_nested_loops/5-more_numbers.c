84f5f30eb58598a38554b73eda65f145781e7875
root <Joshcones1253@yahoo.com>
2023-06-05 19:11:17 -0700

    TECH

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 
 	for (j = 1; j <= 10; j++)
 	{
-		for (c = 0; c <= 14; c++)
-		{
-			if (c >= 10)
-				_putchar('1');
-			_putchar (c % 10 + '0');
-		}
-		_putchar('n');
+	for (c = 0; c <= 14; c++)
+	{
+	if (c >= 10)
+	_putchar('1');
+	_putchar (c % 10 + '0');
+	}
+	_putchar('n');
 	}
 }
