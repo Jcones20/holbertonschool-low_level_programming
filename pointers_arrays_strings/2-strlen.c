f0e148d0d55d2ac04c9c9f239b0a6318a9502d2e
root <Joshcones1253@yahoo.com>
2023-06-05 20:27:54 -0700

    SEATTLE

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ */
+int _strlen(char *s)
+{
+	int best = 0;
+
+	while (*s != '\0')
+	{
+		best++;
+		s++;
+	}
+
+	return (best);
+}
