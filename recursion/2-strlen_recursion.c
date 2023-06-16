d73d5c11866737becd36a9311c2a4a29b0ebcbf6
root <Joshcones1253@yahoo.com>
2023-06-16 11:39:03 -0700

    COWBOYS

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * _strlen_recursion - Return the length of a string.
- * @e: The string to be measure.
+ * @S: The string to be measure.
  *
  * Return: The length of the string.
  */
-int _strlen_recursion(char *e)
+int _strlen_recursion(char *s)
 {
 	int john = 0;
 
-	if (e)
+	if (s)
 	{
 		john++;
-		john += _strlen_recursion(e + 1);
+		john += _strlen_recursion(s +1);
 	}
 	return (john);
 }
