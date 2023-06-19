86f6c32d90c35cc4236109493753d01ccd859ed1
root <Joshcones1253@yahoo.com>
2023-06-19 09:13:53 -0700

    COWBOYS

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -12,13 +12,16 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0);
+	return (actual_sqrt_recursion(n, 0));
 }
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
  *
  * @n: number to calculate the square root
+ * @i: i is the interation
+ *
+ * Return: We returning -1
  */
 int actual_sqrt_recursion(int n, int i)
 {
