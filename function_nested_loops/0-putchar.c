df4892a5666c6095ae2cf3d6b5f3bcb0a54399b3
root <Joshcones1253@yahoo.com>
2023-06-23 11:57:52 -0700

    COWBOYS

--- function_nested_loops/0-putchar.c
+++ function_nested_loops/0-putchar.c
@@ -1,24 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: main.h
- * Auth: William A Meilahn
- * Desc:Header file containing declarations for all functions
- * used in the functions_nested_loops directory.
+#include <unistd.h>
+/**
+ *  _putchar - writes the character c to stdout
+ *  0c: The character to print
+ *
+ *  Return: On success 1.
+ *  On error , -1 is returned, and errno is set appropriately.
  */
-
-int _putchar (char c);
-void print_alphabet(voiid);
-void print_alphabet_x10 (void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-
-#endif
+int _putchar(char c)
+{
+	Return (write(1, c, 1));
