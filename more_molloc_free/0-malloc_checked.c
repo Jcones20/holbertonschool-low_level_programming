12662f81529fee982d37d0da80ed14b38519f740
root <Joshcones1253@yahoo.com>
2023-06-23 11:52:09 -0700

    COWBOYS

--- more_molloc_free/0-malloc_checked.c
+++ more_molloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
- * Return: a pointer to the allocated memory in c
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
