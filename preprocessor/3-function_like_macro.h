1d4cc2222995266edd716404ea4dcd4425ec2e5e
root <Joshcones1253@yahoo.com>
2023-06-24 11:18:24 -0700

    COWBOYS

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (('X') < (0) ? -(x) : (x))
+#define printf("%d\n" , ABS(x) (('X') < (0) ? -(x) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif
