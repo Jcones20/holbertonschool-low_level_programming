d926ecab9b8186a2522522080c4ef6f32ac6d343
root <Joshcones1253@yahoo.com>
2023-06-30 12:38:57 -0700

    COWBOYS

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -20,3 +20,16 @@ int _strlen(char *s)
 
 	return (i);
 }
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * including terminating null bytes (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
