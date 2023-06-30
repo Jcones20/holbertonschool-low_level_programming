3024580640c14bc33d12413a76ad8345c7fb7d02
root <Joshcones1253@yahoo.com>
2023-06-30 10:28:15 -0700

    COWBOYS

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - initializes a variable of type struct dog
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ * Description: Define a new type struct dog with the following elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
