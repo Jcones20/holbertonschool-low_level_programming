d5161ee92e6a5048dd20ba45d336d0424457298e
root <Joshcones1253@yahoo.com>
2023-07-03 13:03:22 -0700

    COWBOYS

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 void array_interator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
