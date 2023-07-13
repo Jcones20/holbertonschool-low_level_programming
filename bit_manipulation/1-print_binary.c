ba52705d4c01212145f010ed0155438aed3aea4d
root <Joshcones1253@yahoo.com>
2023-07-13 13:30:30 -0700

    COWBOYS

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,17 +9,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned int dec_val = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (!b)
-		return (0);
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
-	return (dec_val);
+	_putchar((n & 1) + '0');
 }
-
