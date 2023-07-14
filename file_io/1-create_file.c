4eb484f45405d59ff3a82c8a413297737982e17b
root <Joshcones1253@yahoo.com>
2023-07-14 16:04:36 -0700

    COWBOYS

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_context)
 			len++;
 	}
 
-	fd = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	fd = open(filename, 0_CREAT  0_RDWR  0_TRUNC, 0600);
 
 	w = write(fd, text_context, len);
 
