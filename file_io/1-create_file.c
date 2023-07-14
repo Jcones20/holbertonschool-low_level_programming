d7a94ae2d350b3116bc19c0c1861942d1319e79e
root <Joshcones1253@yahoo.com>
2023-07-14 16:02:16 -0700

    COWBOYS

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_context)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_context != NULL)
 	{
 		for (len = 0; text_context[len];)
 			len++;
