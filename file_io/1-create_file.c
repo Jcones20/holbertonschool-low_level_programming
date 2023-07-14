3ccbf40910d18cc535c3bc2c545bd58eeaa55339
root <Joshcones1253@yahoo.com>
2023-07-14 15:56:37 -0700

    COWBOYS

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_context: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *	Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_context)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_context[len];)
+			len++;
+	}
+
+	fd = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+
+	w = write(fd, text_context, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
