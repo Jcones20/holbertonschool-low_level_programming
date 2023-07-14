a9d3cbeb552c62f539a54a040e0b36c3152e4319
root <Joshcones1253@yahoo.com>
2023-07-14 16:34:14 -0700

    COWBOYS

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,11 +16,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 	ssize_t t;
 
-	fd = open(0-read_textfile, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	bu = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
+	t = read(fd, bu, letters);
 	w = write(STDOUT_FILENO, bu, t);
 
 	free(bu);
