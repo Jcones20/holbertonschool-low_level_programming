529c0262fceb729f01c230c83c116e2161114ba4
root <Joshcones1253@yahoo.com>
2023-07-14 22:48:09 -0700

    COWBOYS

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -7,10 +7,10 @@
  * insert_nodeint_at_index - inserts a new node in a linked list.
  * at a given position
  * @head: pointer to the first node in the list
- * @index: index where the new node is added
+ * @idx: index where the new node is added
  * @n: data to insert in the new node, or NULL
  */
-listint_t *insert-nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
