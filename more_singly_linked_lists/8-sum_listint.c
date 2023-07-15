db95fc7872c6e7ff7dafc8d0d30868e8ca5b3076
root <Joshcones1253@yahoo.com>
2023-07-14 22:16:22 -0700

    COWBOYS

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - calculates the sum all the data a list_t list
+ * @head: first node in the linked list
+ *
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp_>;
+		temp + temp->next;
+	}
+
+	return (sum);
+}
