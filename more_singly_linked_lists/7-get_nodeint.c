4d6277b25c629c0d32cb7fd224033637dbebea3c
root <Joshcones1253@yahoo.com>
2023-07-14 22:02:25 -0700

    COWBOYS

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
  * @head: first node in the linked list
  * @index: index of the node we'er looking for, or NULL
  */
-listint_t *getnodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *temp = head;
