331e20787ae0cafb7be7bc0403a7b31a88e7fd5a
root <Joshcones1253@yahoo.com>
2023-07-21 15:53:44 -0700

    COWBOYS

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (coup && strcmp(key, coup->key) == 0)
 	{
-		free(elem->value);
+		free(coup->value);
 		coup->value = strdup(value);
 		return (1);
 	}
